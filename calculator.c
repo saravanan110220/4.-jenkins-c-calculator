22b404e4dfa34550a4012050ac401b2d7398b812
saravanan110220 <saravanan.r2023a@vitstudent.ac.in>
2026-02-06 22:43:59 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, choice;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Choose operation:\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1:
+            printf("Result: %d\n", a + b);
+            break;
+        case 2:
+            printf("Result: %d\n", a - b);
+            break;
+        case 3:
+            printf("Result: %d\n", a * b);
+            break;
+        case 4:
+            if(b != 0)
+                printf("Result: %d\n", a / b);
+            else
+                printf("Division by zero error\n");
+            break;
+        default:
+            printf("Invalid choice\n");
+    }
+
+    return 0;
+}
